1c70ebf574946da04bb0d240c77b0a9c60997249
Childrey <schildrey2@EMAIL.RADFORD.EDU>
2016-10-15 17:19:44 -0400

    Added log out button

--- homePage.php
+++ homePage.php
@@ -7,6 +7,11 @@
 </head>
 
 <body>
+<form align="right" name="form1" method="post" action="log_out.php">
+  <label class="logoutLblPos">
+  <input name="submit2" type="submit" id="submit2" value="Log Out">
+  </label>
+</form>
 <h1>Betting Pool</h1>
 <div class="createNew">
     <form action="http://google.com">
