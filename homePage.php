43c159f97e8ce3d776350eab152195c1cea2afb6
Childrey <schildrey2@EMAIL.RADFORD.EDU>
2016-10-15 16:52:04 -0400

    Update Home Page

--- homePage.php
+++ homePage.php
@@ -2,12 +2,16 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>Betting Pool Home</title>
 <link href="home.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <h1>Betting Pool</h1>
-<p>&nbsp;</p>
+<div class="createNew">
+    <form action="http://google.com">
+        <input type="submit" value="Create New Betting Pool" />
+    </form>
+</div>
 </body>
 </html>
