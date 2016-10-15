31efc4b3ee56f286e55641f9c400072db000ef6e
jtlane16 <jtlane13@embarqmail.com>
2016-10-15 18:38:49 -0400

    Added Login Files

---
+++ BPP.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="css.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <input type="button" value="Register" style="float: right;">
+  <br/>
+  <h1>Betting Pool </h1>
+    <h3>
+   	 Username: &nbsp; <input type='text' name='Username' size='20' maxlength='100'  id='UsernameField' />
+    <br/>
+    Password: &nbsp;&nbsp; <input type='password' name='Password' size='20' maxlength='100'  id='PasswordField' />
+    <br/>
+    &nbsp;<a href="homePage.php"><input type="button" value="Log In" style="float: center;"/>
+</a>
+  </h3>
+   
+</body>
+</html>
