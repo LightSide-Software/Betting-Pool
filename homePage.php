69b4f5fca172d8d3b08d1682ede3fd74a182958c
Childrey <schildrey2@EMAIL.RADFORD.EDU>
2016-10-15 16:22:29 -0400

    Add home page

---
+++ homePage.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="home.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<h1>Betting Pool</h1>
+<p>&nbsp;</p>
+</body>
+</html>
