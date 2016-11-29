b3cced66d99559484570d8b5f8bb0336c8057a70
Steven Childrey <steven.childrey36@yahoo.com>
2016-11-29 15:22:16 -0500

    Add php file

---
+++ register.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    <meta charset="utf-8">
+    <title>Register Account
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"
+    rel="stylesheet">
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-glyphicons.css"
+    rel="stylesheet">
+	<link href="home.css" rel="stylesheet" type="text/css">
+	<link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/ >
+  </head>
+  
+  <body>
+    <div class="container">
+      <h3>
+      </h3>
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            Register
+          </h3>
+        </div>
+        <div class="panel-body">
+          <div class="form-group">
+            <label>
+              Username
+            </label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>
+              Password
+            </label>
+            <input type="password" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>
+              Email
+            </label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>
+              Phone
+            </label>
+            <input type="number" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>
+              Name
+            </label>
+            <input type="text" class="form-control">
+          </div>
+          <center>
+            <a href="homePage.php"><button type="submit" class="btn btn-success">
+              Register
+            </button></a>
+          </center>
+          &nbsp;
+        </div>
+      </div>
+      <center>
+      </center>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"
+    >
+    </script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"
+    >
+    </script>
+  </body>
+
+</html>
